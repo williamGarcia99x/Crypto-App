5456a1628a50ce491a10d9bd08b3cc543722b59f
William Garcia <wwoodsgarcia@ufl.edu>
2024-09-25 22:10:05 -0400

    Made significant progress on implementation of CoinsTable. Need to adjust the look of the CoinsTable as the viewport grows wider

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,7 +9,7 @@ export const revalidate = 60;
 
 export default async function Home() {
   const data = await getCoinList();
-  console.log(data);
+
   return (
     <main className="">
       <div className="flex flex-col gap-6">
